3e1f297a00a7d6ea291c0e0aa34debc9b42afc0e
kahneraja <ghkpr78>
2014-08-24 10:55:56 +1000

    big buttons and fast click.

--- RedGreenRefactor/www/app/app.js
+++ RedGreenRefactor/www/app/app.js
@@ -1,6 +1,9 @@
 (function () {
     'use strict';
-    var app = angular.module("RedGreenRefactor", ['ngRoute']);
+    var app = angular.module("RedGreenRefactor", ['ngRoute']).
+      run(function () {
+          FastClick.attach(document.body);
+      });;
 
     app.config(['$routeProvider', function ($routeProvider) {
         $routeProvider
