da320945fb69e56d07f7a9dfb2483dae5d6857cb
kahneraja <ghkpr78>
2014-08-25 09:53:00 +1000

    adding social sharing.

--- RedGreenRefactor/www/app/controllers/finishCtrl.js
+++ RedGreenRefactor/www/app/controllers/finishCtrl.js
@@ -4,7 +4,9 @@
     angular.module('RedGreenRefactor').controller('finishCtrl', ['$scope', finishCtrl]);
 
     function finishCtrl($scope) {
-
+        $scope.share = function () {
+            window.plugins.socialsharing.share('Message only');
+        };
     }
 
 }());
